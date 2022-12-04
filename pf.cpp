4094907df3c460425dd7ca8a9fb77016f62b10bb
muhammad shahroz <shahrozimtiaz758@gmail.com>
2022-12-04 07:10:42 -0800

    any changes

---
+++ pf.cpp
@@ -0,0 +1,682 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    cout<<"\n\t\t\t\t\t                          ********************************************   ";
+	cout<<"\n\t\t\t\t\t                          **                                        **  ";
+	cout<<"\n\t\t\t\t\t   *************************                                        ************************* ";
+	cout<<"\n\t\t\t\t\t   * SECURE               **            LOGIN     PAGE              **                VAULT *    ";
+	cout<<"\n\t\t\t\t\t   *************************                   *                    ************************* ";
+	cout<<"\n\t\t\t\t\t                          **                *******                 **   ";
+	cout<<"\n\t\t\t\t\t                          **                                        **    ";
+	cout<<"\n\t\t\t\t\t                          ********************************************   "<<endl;
+	
+	
+    
+    
+    
+    
+    string username;
+    
+	string ronaldo;
+	
+    ronaldo="ronaldo";
+	
+	cout<<"ENTER USERNAME :        ";
+	
+	cin>>username;
+	
+	cout<<"ENTER PASSWORD  :     ";
+	
+	float password;
+	
+	cin>>password;
+
+if (password!=12345678 || username!=ronaldo)
+
+
+{cout<<"\n CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+cout<<"WRONG ID OR PASSWORD!";
+
+Sleep(2000);
+
+system("cls");
+}
+
+
+
+else 
+
+ 	
+{ 
+cout<<"\n CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	cout<<"\n PLEASE CONNECT WITH ALL THE GIVEN VPNS FOR GETTING  SOURCE CODE";
+	
+	cout<<"\n.\t  PROVIDED VPN";
+	
+	cout<<"\n.\t  HOUSE OF BLOOD VPN";
+	
+	cout<<"\n.\t  G.O.A.T VPN";
+	
+	cout<<"\n.\t  ELITE VPN";
+	
+	cout<<"\n\n\t\t\t  STEP 1 :\tPRESS 17 FOR HOUSE OF BLOOD VPN     \n\t\t\t     (NOTICE: WRONG INPUT WILL STOP THE SYSTEM ) \n\t\t\t\t                ";
+	
+	int a;
+	
+	cin>>a;
+	
+	switch(a)
+	{
+		case 17:
+	
+	    cout<<"\nTHIS IS HOUSE OF BLOOD VPN       ";
+	
+	    cout<<"\nPRESS 123 FOR THE  SERVER  MENU    ";
+	
+	    int y;
+	
+	    cin>>y;
+	
+	
+	    if(y!=123)
+	
+	    {	    cout<<"INVALID INPUT";		}
+	
+	    else
+	
+	{    cout<<"\nPRESS 1 FOR INDIAN SERVER   ";
+	
+	    cout<<"\nPRESS 2 FOR MENA SERVER   ";
+	
+	    cout<<"\nPRESS 3 FOR CARABIAN SERVER   ";
+	
+	    cout<<"\nPRESS 4 FOR BRAZIL SERVER   ";
+	
+	    int a;
+	
+	    cin>>a;
+	
+	    switch(a)
+	    p:
+	
+	    {case 1:cout<<"\nYOU ARE CONNECTED WITH INDIAN SERVER";
+	
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	    cout<<"\nSOURCE CODE        213";
+	
+	    break;
+	
+	    case 2:cout<<"\nYOU ARE CONNECTED WITH MENA SERVER";
+	
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	    cout<<"\nSOURCE CODE     213   ";
+	
+	    break;
+	
+	    case 3:cout<<"\nYOU ARE CONNECTED WITH CARABIAN SERVER";
+	
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	
+	    cout<<"\nSOURCE CODE       213 ";
+	
+	    break;
+	
+	    case 4:cout<<"\nYOU ARE CONNECTED WITH BRAZIL SERVER";
+	
+	
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	    cout<<"\nSOURCE CODE       213 ";
+	
+	    break;
+	
+	    default:cout<<"INVALID INPUT";
+	    
+		}
+	}
+	cout<<"\n\n\t\t\t  STEP 2 :\tPRESS 145 FOR G.O.A.T VPN               ";
+
+	int t;
+
+	    cin>>t;
+
+	    if(t!=145)
+
+	    {	    cout<<"INVALID INPUT";		}
+
+	    else
+
+	   {
+
+		 case 145:
+
+	    cout<<"\nTHIS IS  G.O.A.T VPN    ";
+
+	    cout<<"\nPRESS 456 FOR  SERVER MENU   ";
+
+	     int x;
+
+	    cin>>x;
+
+	    if(x!=456)
+
+	    {	    cout<<"INVALID INPUT";		}
+
+	    else
+
+	    {
+		cout<<"\nPRESS 5 FOR RUSSIAN SERVER   ";
+
+	    cout<<"\nPRESS 6 FOR EGYPYIAN SERVER   ";
+
+	    cout<<"\nPRESS 7 FOR KOREAN SERVER   ";
+
+	    cout<<"\nPRESS 8 FOR BRITAIN SERVER   ";
+
+	    int U;
+
+	    cin>>U;
+
+	    switch(U)
+
+	    {case 5:cout<<"\nYOU ARE CONNECTED WITH RUSSIAN SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        234";
+
+	    break;
+
+	    case 6:cout<<"\nYOU ARE CONNECTED WITH  EGYPTIAN SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE       234 ";
+
+	    break;
+
+	    case 7:cout<<"\nYOU ARE CONNECTED WITH  KOREAN SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        234";
+
+	    break;
+
+	    case 8:cout<<"\nYOU ARE CONNECTED WITH BRITAIN SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        234";
+
+	    break;
+
+	    default:cout<<"invalid input";
+
+		}}
+
+		}cout<<"\n\n\t\t\t  STEP 3 :\tPRESS 156 FOR ELITE VPN               ";
+
+	int tt;
+
+	    cin>>tt;
+
+	    if(tt!=156)
+
+	    {	    cout<<"INVALID INPUT";		}
+
+	    else
+
+	{
+			case 356:
+	   
+		 cout<<"\nTHIS IS ELITE VPN    ";
+
+	    cout<<"\nPRESS 897 FOR  SERVER MENU   ";
+
+	     int E;
+
+	    cin>>E;
+
+	    if(E!=897)
+
+	   {
+		 	    cout<<"INVALID INPUT";		}
+	    else
+
+	    {
+
+		cout<<"\nPRESS 11 FOR  AFRIACN SERVER   ";
+
+	    cout<<"\nPRESS 12 FOR  CHINA SERVER   ";
+
+	    cout<<"\nPRESS 13 FOR  SPANISH SERVER   ";
+
+	    cout<<"\nPRESS 14 FOR AUSTRAILIA SERVER   ";
+
+	    int U;
+
+	    cin>>U;
+
+	    switch(U)
+//	p:  /* p as a used for  goto statement */
+	    {case 11:cout<<"\nYOU ARE CONNECTED WITH AFRICAN SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        478";
+
+	    break;
+
+	    case 12:cout<<"\nYOU ARE CONNECTED WITH  CHINA SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+	    
+	    cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        478 ";
+
+	    break;
+
+	    case 13:cout<<"\nYOU ARE CONNECTED WITH  SPANISH SERVER";
+
+	    cout<<"\nVPN ACTIVATED";
+
+cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE         478";
+
+	    break;
+
+	    case 14:cout<<"\nYOU ARE CONNECTED WITH AUSTRAILIAN SERVER";
+	    
+
+		cout<<"\nVPN ACTIVATED";
+		
+		cout<<" \nGENERATING SOURCE CODE ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	    cout<<"\nSOURCE CODE        478";
+
+	    break;
+
+	    default:cout<<"INVALID INPUT";		}
+
+			break;
+		}
+		
+		}
+	       
+	    
+		
+		
+			
+			
+	
+	
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+					cout<<" \nVPN CONNECTION CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+				    cout<<"\n\nPLEASE INPUT ALL SERVERS SOURCE CODE (NOTICE! SOURCE CODES SHOUD BE IN A ROW BY THIER SEQUENCE WITHOUT ANY SPACES :       ";
+
+	    int ab;
+
+	    cin>>ab;
+
+	    if(ab!=213234478)
+
+	    {cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+		cout<<"CHECK VPN CONNECTION";
+		system("cls");
+//		goto()
+		}
+
+		else
+
+		{int a;
+		cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+            cout<<"\n YOU ARE CONNECTED WITH ALL VPNS";
+			cout<<"\nPLEASE ENTER  1 0 1 TO CONTINUE ";
+
+			cin>>a;
+
+			if(a!=101)
+
+			{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+			cout<<"INVALID INPUT";
+
+					Sleep(2000);
+
+			system("cls");
+
+			}
+
+			else
+
+{  cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;  
+
+    string username;
+    
+	string redrooms;
+	
+	redrooms="redrooms";
+//	p:
+	cout<<"ENTER USERNAME :        ";
+	
+	cin>>username;
+	
+	cout<<"ENTER PASSWORD  :     ";
+	
+	float password;
+	
+	cin>>password;
+	
+if (password!=12332123 || username!=redrooms)
+
+
+{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+cout<<"WRONG ID OR PASSWORD!";
+
+		Sleep(2000);
+
+			system("cls");}
+// 			goto();
+else 	
+
+{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;  
+
+//next step recapture
+
+ cout<<"\n FILL RECAPTCH  "<<endl;
+
+
+Sleep(1000);
+
+    cout<<"                                                      *        *        "<<endl;
+    cout<<"                                                     ***      ***       "<<endl;  
+    cout<<"                                                           *            "<<endl;
+    cout<<"                                                    *     ***    *      "<<endl;	
+	cout<<"                                                   ***          ***     "<<endl;
+	
+	
+	float a;
+
+	string tra;
+
+	string triangle;
+
+	triangle="triangle";
+
+	cout<<"\nENTER THE  NAME OF SHAPE      ";
+
+	cin>>tra;
+
+	cout<<"ENTER THE TOTAL NO.SHAPES       ";
+
+	cin>>a;
+
+	if(a!=5 || tra!=triangle)
+
+	{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	cout<<"RECAPTCH FAIL      ";
+
+			Sleep(2000);
+
+			system("cls");
+
+	}
+
+	else
+
+	{
+
+	cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+	cout<<"\nRECAPTCH SUCCESS    ";
+
+	Sleep(1000);
+
+cout<<"\n WELCOME TO SECURE VAULT  ";
+
+cout<<"\n PLEASE READ OUT THE DETAILS CAREFULLY";
+
+cout<<"\n                                              DETAILS ";
+
+cout<<"\n NOTICE!: THIS BROWSER IS BASED ON TOR BROWSER,IT WILL CHANGE AND HIDE YOUR IP ADDRESS ";
+
+cout<<"\n NOW YOU ARE CONNECTED WITH RANDOM SERVER WHICH IS PROVIDED BY THIS BROWSER";
+
+
+ long int rubber;
+
+	cout<<"\nNOTICE!           SEARCH A VALID LINK";
+
+   	cout<<"\n_______________________________________________________________________________________________";
+
+	cout<<"\n|SEARCH| : ";
+
+
+	cin>>rubber;
+
+	if(rubber==45235678)
+
+	{cout<<"LOADING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	cout<<"                                                                       SECURE VAULT          "<<endl;
+    cout<<"\n                                 PLEASE LOGIN TO CONTINUE"<<endl;
+    
+    
+    
+    string username;
+    
+	string dark;
+	
+	dark="dark";
+	
+	cout<<"ENTER USERNAME :        ";
+	
+	cin>>username;
+	
+	cout<<"ENTER PASSWORD  :     ";
+	
+	float password;
+	
+	cin>>password;
+	
+if (password!=87654321||username!=dark)
+
+{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+cout<<"WRONG ID OR PASSWORD!";
+
+		Sleep(2000);
+
+			system("cls");
+}
+
+else 
+
+	{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+
+	int sc,sn,su;
+
+cout<<"ENTER SOURCE X CODE           ";cin>>sc;
+
+cout<<"ENTER SERIAL NUMBER         ";cin>>sn;
+
+cout<<"ENTER SECURITY X UNICODE      ";cin>>su;
+
+if(sc==17897&&sn==14897&&su==26451)
+
+{ 
+cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";
+	
+	cout<<"\n\n\n\nPRIVATE VAULT ACCESS"<<endl;
+	
+	cout<<"\n 30 KILO GRAM COCAIN WILL BE DELIVERED AT XYZ LOCATION BUT BEFORE THE SERVICE \t\t PAY FOR THE ITEM";
+	
+	cout<<"\n THE ITEM EILL BE DELIVERED WITHIN A WEAK AFTER THE TRANSECTION OF MONEY FROM YOUR ACCOUNTt WE WILL DELIVER IT AND PROVIDE YO INFORMATION AT RANDOM TIME FOR SECURITY PURPOSE";
+	
+	cout<<"\n\n PAYMENT METHOD : BITCOINS";
+	
+	cout<<"\n\n ITEM PRICE : 10000 bitcoins";
+	
+	cout<<"\n\n BITCOIN WALLET NUMBER ..FIXED";
+	
+	cout<<"\n\n\n\n PRESS ";
+	
+	cout<<"\n1.PROCEED TO PAYMENT";
+	
+	cout<<"\n2.EXIT   "<<endl;
+    
+	Sleep(2000);
+    
+	int pp;
+    
+	cin>>pp;
+    
+	if(pp==2)
+    
+	{cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	cout<<"BROWSER CLOSING";
+	
+			Sleep(2000);
+	
+			system("cls");
+	}
+	
+	else if(pp==1)
+	
+	{cout<<"LOADING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+	
+	cout<<"\n ENTER BITCOINS : ";
+	
+			int ni;
+	cin>>ni;
+	if(ni==10000)
+	{
+	int array[7000];
+	for(int i=0;i<7000;i++)
+	{	 
+	cout<<"............................  ";
+	
+		 
+		 
+	}
+	
+    for(int i=0;i<1000;i++)
+	{
+	
+ 	    if(array[0]< array[i])
+ 	    
+    	array[0]=array[i];
+	
+	}
+	cout<<"\n AMOUNT PAYED TO WALLET NUMBER= 3.23"<<array[0];
+	
+//	tomorrow i will cheak here
+	for(int i=0;i<10000;i++)
+	{
+		
+ 	    if(array[0]>= array[i])
+ 	    
+    	array[0]=array[i];
+	
+	}	
+	cout<<"\n YOUR WALLET NUMBER = 0.32"<<array[0];
+	cout<<"\nPAYMENT SUCCESFULL";
+	 Sleep(5000);
+		system("cls");
+		
+	}else
+	{cout<<"PLEASE  CHECK THE AMOUNT";
+	Sleep(2000);
+	system("cls");
+	}
+	}
+	else
+	{cout<<"INVALID";
+	Sleep(2000);
+		system("cls");
+	}
+}
+else
+{ cout<<"CHECKING ";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<".";Sleep(1000);cout<<"."<<endl;
+
+cout<<"\nYOU CANT EXPLORE THIS SITE";
+		Sleep(3000);
+			system("cls");
+}
+
+
+	}
+	
+
+
+	}	
+	
+
+	
+	
+	
+		
+		else
+{Sleep(1000);
+cout<<"INVALID";
+		Sleep(3000);
+			system("cls");
+	}	
+	
+	}
+
+
+
+
+}
+}
+}
+			
+}
+getch();
+return 0;
+	}
